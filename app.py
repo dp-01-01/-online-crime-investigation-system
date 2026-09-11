e8d6c476ea3e3d3afa052d48aabeeb4cf7706d39
Durga prasad <prasad02485@gmail.com>
2026-09-11 14:29:35 +0530

    Create website home page

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Online Crime Investigation System"
+    return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return "Login page coming soon..."
 
 
 if __name__ == "__main__":
